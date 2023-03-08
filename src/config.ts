9ae789b0c55d6b280892c092682ac12a368006a8
Harri <harri.h96@gmail.com>
2023-03-08 12:25:50 +0200

    add search, translation support

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,57 @@
 import axios from "axios";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+export const translations = i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: {
+          main: {
+            language: "Language",
+          },
+          search: {
+            title: "Lounasehdotus",
+            description: "Enter city and search restaurants",
+            placeholder: "City",
+          },
+          lunchList: {
+            return: "City selection",
+            location: "Show location",
+          },
+        },
+      },
+      fi: {
+        translation: {
+          main: {
+            language: "Kieli",
+          },
+          search: {
+            title: "Lounasehdotus",
+            description: "Syötä kaupunki ja hae ravintolat",
+            placeholder: "Kaupunki",
+          },
+          lunchList: {
+            return: "Kaupungin valinta",
+            location: "Näytä sijainti",
+          },
+        },
+      },
+    },
+  });
+
+export const changeLanguage = () => {
+  i18n.changeLanguage(i18n.resolvedLanguage === "en" ? "fi" : "en");
+};
--- src/views/MainView/index.tsx
+++ src/views/MainView/index.tsx
@@ -4,13 +4,20 @@ import {
   AccordionActions,
   AccordionDetails,
   AccordionSummary,
+  Button,
+  CircularProgress,
+  FormControl,
   Grid,
+  IconButton,
+  TextField,
   Typography,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { api } from "../../config";
+import { api, changeLanguage } from "../../config";
 import { DateTime } from "luxon";
 import { AxiosResponse } from "axios";
+import { Place, Search, Translate } from "@mui/icons-material";
+import { useTranslation } from "react-i18next";
 
 type Restaurant = {
   id: number;
@@ -28,57 +35,136 @@ type Dish = {
 };
 
 const MainView = () => {
+  const [city, setCity] = useState("");
+  const [date, setDate] = useState(DateTime.now());
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [showList, setShowList] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { t } = useTranslation();
 
   const getLunchList = async () => {
     setRestaurants([]);
+    setShowList(false);
     setLoading(true);
     api
-      .get("/lunches?city=kuopio&date=" + DateTime.now().toFormat("yyyy-LL-dd"))
+      .get(`/lunches?city=${city}&date=${DateTime.now().toFormat("yyyy-LL-dd")}`)
       .then((res: AxiosResponse<Array<Restaurant>>) => {
         setRestaurants(res.data);
         setLoading(false);
+        setShowList(true);
       })
       .catch((err: Error) => {
-        console.log(err);
+        console.error(err);
         setLoading(false);
       });
   };
 
-  useEffect(() => {
-    getLunchList();
-  }, []);
+  const clearList = () => {
+    setCity("");
+    setRestaurants([]);
+    setShowList(false);
+  };
+
+  const mapsLink = (address: string) => {
+    const url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+      address
+    )}`;
+    window.open(url, "_blank");
+  };
 
   return (
     <React.Fragment>
-      <Grid container>
-        {loading && (
-          <Grid item xs={12}>
-            Ladataan...
+      <Grid container spacing={1}>
+        <IconButton
+          style={{ position: "absolute", right: 0, top: 0 }}
+          title={t("translation:main.language") as string}
+          onClick={() => changeLanguage()}
+        >
+          <Translate />
+        </IconButton>
+        {!showList && (
+          <Grid container item xs={12} spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="h4">{t("translation:search.title")}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              {t("translation:search.description")}
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl variant="outlined">
+                <TextField
+                  placeholder={t("translation:search.placeholder") as string}
+                  aria-label="city"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      getLunchList();
+                      e.preventDefault();
+                    }
+                  }}
+                  InputProps={{
+                    endAdornment: !loading ? (
+                      <IconButton aria-label="search" onClick={() => getLunchList()}>
+                        <Search />
+                      </IconButton>
+                    ) : (
+                      <CircularProgress size={20} />
+                    ),
+                  }}
+                />
+              </FormControl>
+            </Grid>
           </Grid>
         )}
 
-        {restaurants &&
-          restaurants.map((restaurant) => (
-            <Grid item xs={12} key={restaurant.id}>
-              <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                  <Typography variant="subtitle2">{restaurant.name}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  {restaurant.dishes.map((dish: any) => (
-                    <Typography variant="body1" key={dish.id}>
-                      {dish.name}
-                    </Typography>
-                  ))}
-                </AccordionDetails>
-                <AccordionActions>
-                  <Typography variant="body2">{restaurant.address}</Typography>
-                </AccordionActions>
-              </Accordion>
+        {showList && (
+          <>
+            <Grid item xs={12}>
+              <Typography variant="h6">
+                {city[0].toUpperCase() + city.slice(1)} {date.toFormat("m.d.yyyy")}
+              </Typography>
             </Grid>
-          ))}
+            {restaurants
+              .filter((r) => r.dishes.length)
+              .map((restaurant) => (
+                <Grid item xs={12} key={restaurant.id}>
+                  <Accordion>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                      <Typography variant="subtitle2">{restaurant.name}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      {restaurant.dishes.map((dish: any) => (
+                        <Typography variant="body1" key={dish.id}>
+                          {dish.name}
+                        </Typography>
+                      ))}
+                    </AccordionDetails>
+                    <AccordionActions>
+                      {restaurant.address && (
+                        <IconButton
+                          onClick={() => mapsLink(restaurant.address)}
+                          title={t("translation:lunchList.location") as string}
+                          style={{ color: "red" }}
+                        >
+                          <Place />
+                        </IconButton>
+                      )}
+                    </AccordionActions>
+                  </Accordion>
+                </Grid>
+              ))}
+          </>
+        )}
+
+        {showList && (
+          <Grid item xs={12} style={{ textAlign: "right" }}>
+            <Button variant="text" onClick={() => clearList()}>
+              <small>{t("translation:lunchList.return")}</small>
+            </Button>
+          </Grid>
+        )}
       </Grid>
     </React.Fragment>
   );
